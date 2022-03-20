8fa3980c21e68e7b30d03323a1b9e9f8b0eb6916
Afonso Henrique <89816770+aefonso@users.noreply.github.com>
2022-03-20 14:07:27 -0300

    Editando o gerenciamento de comida

--- admin/manage-food.php
+++ admin/manage-food.php
@@ -2,12 +2,12 @@
 
 <div class="main-content">
     <div class="wrapper">
-        <h1>Manage Food</h1>
+        <h1>Gerenciamento</h1>
 
         <br /><br />
 
-                <!-- Button to Add Admin -->
-                <a href="<?php echo SITEURL; ?>admin/add-food.php" class="btn-primary">Add Food</a>
+                
+                <a href="<?php echo SITEURL; ?>admin/add-food.php" class="btn-primary">Adicionar Comida</a>
 
                 <br /><br /><br />
 
@@ -46,35 +46,34 @@
 
                 <table class="tbl-full">
                     <tr>
-                        <th>S.N.</th>
-                        <th>Title</th>
-                        <th>Price</th>
-                        <th>Image</th>
-                        <th>Featured</th>
-                        <th>Active</th>
-                        <th>Actions</th>
+                        <th>Código</th>
+                        <th>Título</th>
+                        <th>Preço</th>
+                        <th>Imagem</th>
+                        <th>Destaque</th>
+                        <th>Ativo</th>
+                        <th>Ações</th>
                     </tr>
 
                     <?php 
-                        //Create a SQL Query to Get all the Food
+                       
                         $sql = "SELECT * FROM tbl_food";
 
-                        //Execute the qUery
+                        
                         $res = mysqli_query($conn, $sql);
 
-                        //Count Rows to check whether we have foods or not
+                        
                         $count = mysqli_num_rows($res);
 
-                        //Create Serial Number VAriable and Set Default VAlue as 1
+                        
                         $sn=1;
 
                         if($count>0)
                         {
-                            //We have food in Database
-                            //Get the Foods from Database and Display
+                            
                             while($row=mysqli_fetch_assoc($res))
                             {
-                                //get the values from individual columns
+                                
                                 $id = $row['id'];
                                 $title = $row['title'];
                                 $price = $row['price'];
@@ -89,15 +88,15 @@
                                     <td>$<?php echo $price; ?></td>
                                     <td>
                                         <?php  
-                                            //CHeck whether we have image or not
+                                            
                                             if($image_name=="")
                                             {
-                                                //WE do not have image, DIslpay Error Message
-                                                echo "<div class='error'>Image not Added.</div>";
+                                                
+                                                echo "<div class='error'>Imagem não foi Adicionada.</div>";
                                             }
                                             else
                                             {
-                                                //WE Have Image, Display Image
+                                                
                                                 ?>
                                                 <img src="<?php echo SITEURL; ?>images/food/<?php echo $image_name; ?>" width="100px">
                                                 <?php
@@ -107,8 +106,8 @@
                                     <td><?php echo $featured; ?></td>
                                     <td><?php echo $active; ?></td>
                                     <td>
-                                        <a href="<?php echo SITEURL; ?>admin/update-food.php?id=<?php echo $id; ?>" class="btn-secondary">Update Food</a>
-                                        <a href="<?php echo SITEURL; ?>admin/delete-food.php?id=<?php echo $id; ?>&image_name=<?php echo $image_name; ?>" class="btn-danger">Delete Food</a>
+                                        <a href="<?php echo SITEURL; ?>admin/update-food.php?id=<?php echo $id; ?>" class="btn-secondary">Atualizar</a>
+                                        <a href="<?php echo SITEURL; ?>admin/delete-food.php?id=<?php echo $id; ?>&image_name=<?php echo $image_name; ?>" class="btn-danger">Deletar</a>
                                     </td>
                                 </tr>
 
@@ -117,8 +116,8 @@
                         }
                         else
                         {
-                            //Food not Added in Database
-                            echo "<tr> <td colspan='7' class='error'> Food not Added Yet. </td> </tr>";
+                          
+                            echo "<tr> <td colspan='7' class='error'> Comida não adicionada. </td> </tr>";
                         }
 
                     ?>
@@ -128,5 +127,3 @@
     </div>
     
 </div>
-
-<?php include('partials/footer.php'); ?>
