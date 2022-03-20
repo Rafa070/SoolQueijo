f98a835136ba78e5930e8278571204abf338c4b1
Afonso Henrique <89816770+aefonso@users.noreply.github.com>
2022-03-20 13:34:17 -0300

    pesquisar comida

--- food-search.php
+++ food-search.php
@@ -1,111 +1,106 @@
 
     <?php include('partials-front/menu.php'); ?>
 
-    <!-- fOOD sEARCH Section Starts Here -->
-    <section class="food-search text-center">
-        <div class="container">
-            <?php 
+<section class="food-search text-center">
+    <div class="container">
+        <?php 
 
-                //Get the Search Keyword
-                // $search = $_POST['search'];
-                $search = mysqli_real_escape_string($conn, $_POST['search']);
+        
+            $search = mysqli_real_escape_string($conn, $_POST['search']);
+        
+        ?>
+
+
+        <h2>Buscou por: <a href="#" class="text-white">"<?php echo $search; ?>"</a></h2>
+
+    </div>
+</section>
+
+
+
+
+
+<section class="food-menu">
+    <div class="container">
+        <h2 class="text-center">Menu</h2>
+
+        <?php 
+
+    
+            $sql = "SELECT * FROM tbl_food WHERE title LIKE '%$search%' OR description LIKE '%$search%'";
+
             
-            ?>
+            $res = mysqli_query($conn, $sql);
 
+            
+            $count = mysqli_num_rows($res);
 
-            <h2>Foods on Your Search <a href="#" class="text-white">"<?php echo $search; ?>"</a></h2>
-
-        </div>
-    </section>
-    <!-- fOOD sEARCH Section Ends Here -->
-
-
-
-    <!-- fOOD MEnu Section Starts Here -->
-    <section class="food-menu">
-        <div class="container">
-            <h2 class="text-center">Food Menu</h2>
-
-            <?php 
-
-                //SQL Query to Get foods based on search keyword
-                //$search = burger '; DROP database name;
-                // "SELECT * FROM tbl_food WHERE title LIKE '%burger'%' OR description LIKE '%burger%'";
-                $sql = "SELECT * FROM tbl_food WHERE title LIKE '%$search%' OR description LIKE '%$search%'";
-
-                //Execute the Query
-                $res = mysqli_query($conn, $sql);
-
-                //Count Rows
-                $count = mysqli_num_rows($res);
-
-                //Check whether food available of not
-                if($count>0)
+         
+            if($count>0)
+            {
+               
+                while($row=mysqli_fetch_assoc($res))
                 {
-                    //Food Available
-                    while($row=mysqli_fetch_assoc($res))
-                    {
-                        //Get the details
-                        $id = $row['id'];
-                        $title = $row['title'];
-                        $price = $row['price'];
-                        $description = $row['description'];
-                        $image_name = $row['image_name'];
-                        ?>
+                   
+                    $id = $row['id'];
+                    $title = $row['title'];
+                    $price = $row['price'];
+                    $description = $row['description'];
+                    $image_name = $row['image_name'];
+                    ?>
 
-                        <div class="food-menu-box">
-                            <div class="food-menu-img">
-                                <?php 
-                                    // Check whether image name is available or not
-                                    if($image_name=="")
-                                    {
-                                        //Image not Available
-                                        echo "<div class='error'>Image not Available.</div>";
-                                    }
-                                    else
-                                    {
-                                        //Image Available
-                                        ?>
-                                        <img src="<?php echo SITEURL; ?>images/food/<?php echo $image_name; ?>" alt="Chicke Hawain Pizza" class="img-responsive img-curve">
-                                        <?php 
-
-                                    }
-                                ?>
+                    <div class="food-menu-box">
+                        <div class="food-menu-img">
+                            <?php 
                                 
-                            </div>
+                                if($image_name=="")
+                                {
+                                    
+                                    echo "<div class='error'>Image not Available.</div>";
+                                }
+                                else
+                                {
+                                
+                                    ?>
+                                    <img src="<?php echo SITEURL; ?>images/food/<?php echo $image_name; ?>" alt="Chicke Hawain Pizza" class="img-responsive img-curve">
+                                    <?php 
 
-                            <div class="food-menu-desc">
-                                <h4><?php echo $title; ?></h4>
-                                <p class="food-price">$<?php echo $price; ?></p>
-                                <p class="food-detail">
-                                    <?php echo $description; ?>
-                                </p>
-                                <br>
-
-                                <a href="#" class="btn btn-primary">Order Now</a>
-                            </div>
+                                }
+                            ?>
+                            
                         </div>
 
-                        <?php
-                    }
+                        <div class="food-menu-desc">
+                            <h4><?php echo $title; ?></h4>
+                            <p class="food-price">$<?php echo $price; ?></p>
+                            <p class="food-detail">
+                                <?php echo $description; ?>
+                            </p>
+                            <br>
+
+                            <a href="#" class="btn btn-primary">Order Now</a>
+                        </div>
+                    </div>
+
+                    <?php
                 }
-                else
-                {
-                    //Food Not Available
-                    echo "<div class='error'>Food not found.</div>";
-                }
-            
-            ?>
+            }
+            else
+            {
+                echo "<div class='error'>Comida não encontrada.</div>";
+            }
+        
+        ?>
 
-            
+        
 
-            <div class="clearfix"></div>
+        <div class="clearfix"></div>
 
-            
+        
 
-        </div>
+    </div>
 
-    </section>
-    <!-- fOOD Menu Section Ends Here -->
+</section>
 
-    <?php include('partials-front/footer.php'); ?>
+
+<?php include('partials-front/footer.php'); ?>
