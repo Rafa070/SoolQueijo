48544c45d6028adcbf5221b5f465f69a700308f7
Rafael Ribeiro <rafaelribeirorn5@gmail.com>
2022-03-20 14:57:35 -0300

    ajustes na tela principal

--- .history/admin/partials/footer_20220320141551.php
+++
@@ -1,9 +0,0 @@
-
-<div class="footer">
-            <div class="wrapper">
-                <p class="text-center"><a href="#">SoolQueijo</a></p>
-            </div>
-        </div>
-
-    </body>
-</html>
--- admin/manage-admin.php
+++ admin/manage-admin.php
@@ -4,7 +4,7 @@
       
         <div class="main-content">
             <div class="wrapper">
-                <h1>Manage Admin</h1>
+                <h1>Controle de Acesso</h1>
 
                 <br />
 
@@ -48,49 +48,38 @@
                 ?>
                 <br><br><br>
 
-                <!-- Button to Add Admin -->
-                <a href="add-admin.php" class="btn-primary">Add Admin</a>
+                <a href="add-admin.php" class="btn-primary">Adicionar Administrador</a>
 
                 <br /><br /><br />
 
                 <table class="tbl-full">
                     <tr>
-                        <th>S.N.</th>
-                        <th>Full Name</th>
-                        <th>Username</th>
-                        <th>Actions</th>
+                        <th>ID</th>
+                        <th>Nome</th>
+                        <th>Usuário</th>
+                        <th>Acões</th>
                     </tr>
-
-                    
+           
                     <?php 
-                        //Query to Get all Admin
+                      
                         $sql = "SELECT * FROM tbl_admin";
-                        //Execute the Query
+                       
                         $res = mysqli_query($conn, $sql);
 
-                        //CHeck whether the Query is Executed of Not
                         if($res==TRUE)
-                        {
-                            // Count Rows to CHeck whether we have data in database or not
-                            $count = mysqli_num_rows($res); // Function to get all the rows in database
+                        {                       
+                            $count = mysqli_num_rows($res);
 
-                            $sn=1; //Create a Variable and Assign the value
+                            $sn=1;
 
-                            //CHeck the num of rows
                             if($count>0)
                             {
-                                //WE HAve data in database
                                 while($rows=mysqli_fetch_assoc($res))
                                 {
-                                    //Using While loop to get all the data from database.
-                                    //And while loop will run as long as we have data in database
-
-                                    //Get individual DAta
                                     $id=$rows['id'];
                                     $full_name=$rows['full_name'];
                                     $username=$rows['username'];
 
-                                    //Display the Values in our Table
                                     ?>
                                     
                                     <tr>
@@ -98,9 +87,9 @@
                                         <td><?php echo $full_name; ?></td>
                                         <td><?php echo $username; ?></td>
                                         <td>
-                                            <a href="<?php echo SITEURL; ?>admin/update-password.php?id=<?php echo $id; ?>" class="btn-primary">Change Password</a>
-                                            <a href="<?php echo SITEURL; ?>admin/update-admin.php?id=<?php echo $id; ?>" class="btn-secondary">Update Admin</a>
-                                            <a href="<?php echo SITEURL; ?>admin/delete-admin.php?id=<?php echo $id; ?>" class="btn-danger">Delete Admin</a>
+                                            <a href="<?php echo SITEURL; ?>admin/update-password.php?id=<?php echo $id; ?>" class="btn-primary">Alterar Senha</a>
+                                            <a href="<?php echo SITEURL; ?>admin/update-admin.php?id=<?php echo $id; ?>" class="btn-secondary">Atualizar Administrador</a>
+                                            <a href="<?php echo SITEURL; ?>admin/delete-admin.php?id=<?php echo $id; ?>" class="btn-danger">Deletar Administrador</a>
                                         </td>
                                     </tr>
 
