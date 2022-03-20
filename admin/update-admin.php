f4322b20b179bc3dd46667659e8e1a6593c86382
Afonso Henrique <89816770+aefonso@users.noreply.github.com>
2022-03-20 14:35:49 -0300

    Atualizar funcionario

--- admin/update-admin.php
+++ admin/update-admin.php
@@ -2,30 +2,29 @@
 
 <div class="main-content">
     <div class="wrapper">
-        <h1>Update Admin</h1>
+        <h1>Atualizar Funcionario</h1>
 
         <br><br>
 
         <?php 
-            //1. Get the ID of Selected Admin
+            
             $id=$_GET['id'];
 
-            //2. Create SQL Query to Get the Details
+            
             $sql="SELECT * FROM tbl_admin WHERE id=$id";
 
-            //Execute the Query
+           
             $res=mysqli_query($conn, $sql);
 
-            //Check whether the query is executed or not
+            
             if($res==true)
             {
-                // Check whether the data is available or not
+                
                 $count = mysqli_num_rows($res);
-                //Check whether we have admin data or not
+                
                 if($count==1)
                 {
-                    // Get the Details
-                    //echo "Admin Available";
+                    
                     $row=mysqli_fetch_assoc($res);
 
                     $full_name = $row['full_name'];
@@ -33,7 +32,7 @@
                 }
                 else
                 {
-                    //Redirect to Manage Admin PAge
+                    
                     header('location:'.SITEURL.'admin/manage-admin.php');
                 }
             }
@@ -45,14 +44,14 @@
 
             <table class="tbl-30">
                 <tr>
-                    <td>Full Name: </td>
+                    <td>Nome Completo: </td>
                     <td>
                         <input type="text" name="full_name" value="<?php echo $full_name; ?>">
                     </td>
                 </tr>
 
                 <tr>
-                    <td>Username: </td>
+                    <td>Nome de usuário: </td>
                     <td>
                         <input type="text" name="username" value="<?php echo $username; ?>">
                     </td>
@@ -61,7 +60,7 @@
                 <tr>
                     <td colspan="2">
                         <input type="hidden" name="id" value="<?php echo $id; ?>">
-                        <input type="submit" name="submit" value="Update Admin" class="btn-secondary">
+                        <input type="submit" name="submit" value="Atualizar" class="btn-secondary">
                     </td>
                 </tr>
 
@@ -73,38 +72,37 @@
 
 <?php 
 
-    //Check whether the Submit Button is Clicked or not
+    
     if(isset($_POST['submit']))
     {
-        //echo "Button CLicked";
-        //Get all the values from form to update
+        
         $id = $_POST['id'];
         $full_name = $_POST['full_name'];
         $username = $_POST['username'];
 
-        //Create a SQL Query to Update Admin
+        
         $sql = "UPDATE tbl_admin SET
         full_name = '$full_name',
         username = '$username' 
         WHERE id='$id'
         ";
 
-        //Execute the Query
+       
         $res = mysqli_query($conn, $sql);
 
-        //Check whether the query executed successfully or not
+        
         if($res==true)
         {
-            //Query Executed and Admin Updated
+            
             $_SESSION['update'] = "<div class='success'>Admin Updated Successfully.</div>";
-            //Redirect to Manage Admin Page
+            
             header('location:'.SITEURL.'admin/manage-admin.php');
         }
         else
         {
-            //Failed to Update Admin
+            
             $_SESSION['update'] = "<div class='error'>Failed to Delete Admin.</div>";
-            //Redirect to Manage Admin Page
+            
             header('location:'.SITEURL.'admin/manage-admin.php');
         }
     }
