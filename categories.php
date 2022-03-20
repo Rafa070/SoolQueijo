b80277b862df5cb7c89dfbe6301443485c6e1458
Afonso Henrique <89816770+aefonso@users.noreply.github.com>
2022-03-20 13:27:53 -0300

    atualizando categorias

--- categories.php
+++ categories.php
@@ -3,73 +3,73 @@
 
 
 
-    <!-- CAtegories Section Starts Here -->
-    <section class="categories">
-        <div class="container">
-            <h2 class="text-center">Explore Foods</h2>
+<!-- CAtegories Section Starts Here -->
+<section class="categories">
+    <div class="container">
+        <h2 class="text-center">Explorar Comidas</h2>
 
-            <?php 
+        <?php 
 
-                //Display all the cateories that are active
-                //Sql Query
-                $sql = "SELECT * FROM tbl_category WHERE active='Yes'";
+            //Display all the cateories that are active
+            //Sql Query
+            $sql = "SELECT * FROM tbl_category WHERE active='Yes'";
 
-                //Execute the Query
-                $res = mysqli_query($conn, $sql);
+            //Execute the Query
+            $res = mysqli_query($conn, $sql);
 
-                //Count Rows
-                $count = mysqli_num_rows($res);
+            //Count Rows
+            $count = mysqli_num_rows($res);
 
-                //CHeck whether categories available or not
-                if($count>0)
+            //CHeck whether categories available or not
+            if($count>0)
+            {
+                //CAtegories Available
+                while($row=mysqli_fetch_assoc($res))
                 {
-                    //CAtegories Available
-                    while($row=mysqli_fetch_assoc($res))
-                    {
-                        //Get the Values
-                        $id = $row['id'];
-                        $title = $row['title'];
-                        $image_name = $row['image_name'];
-                        ?>
-                        
-                        <a href="<?php echo SITEURL; ?>category-foods.php?category_id=<?php echo $id; ?>">
-                            <div class="box-3 float-container">
-                                <?php 
-                                    if($image_name=="")
-                                    {
-                                        //Image not Available
-                                        echo "<div class='error'>Image not found.</div>";
-                                    }
-                                    else
-                                    {
-                                        //Image Available
-                                        ?>
-                                        <img src="<?php echo SITEURL; ?>images/category/<?php echo $image_name; ?>" alt="Pizza" class="img-responsive img-curve">
-                                        <?php
-                                    }
-                                ?>
-                                
+                    //Get the Values
+                    $id = $row['id'];
+                    $title = $row['title'];
+                    $image_name = $row['image_name'];
+                    ?>
+                    
+                    <a href="<?php echo SITEURL; ?>category-foods.php?category_id=<?php echo $id; ?>">
+                        <div class="box-3 float-container">
+                            <?php 
+                                if($image_name=="")
+                                {
+                                    //Image not Available
+                                    echo "<div class='error'>Imagem não encontrada.</div>";
+                                }
+                                else
+                                {
+                                    //Image Available
+                                    ?>
+                                    <img src="<?php echo SITEURL; ?>images/category/<?php echo $image_name; ?>" alt="Pizza" class="img-responsive img-curve">
+                                    <?php
+                                }
+                            ?>
+                            
 
-                                <h3 class="float-text text-white"><?php echo $title; ?></h3>
-                            </div>
-                        </a>
+                            <h3 class="float-text text-white"><?php echo $title; ?></h3>
+                        </div>
+                    </a>
 
-                        <?php
-                    }
+                    <?php
                 }
-                else
-                {
-                    //CAtegories Not Available
-                    echo "<div class='error'>Category not found.</div>";
-                }
-            
-            ?>
-            
+            }
+            else
+            {
+           
+                echo "<div class='error'>Categoria não encontrada.</div>";
+            }
+        
+        ?>
+        
 
-            <div class="clearfix"></div>
-        </div>
-    </section>
-    <!-- Categories Section Ends Here -->
+        <div class="clearfix"></div>
+    </div>
+</section>
 
 
-    <?php include('partials-front/footer.php'); ?>
+
+<?php include('partials-front/footer.php'); ?>
