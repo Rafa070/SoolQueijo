f9cd2b689a76ec8ad851142e40873b6a10acefca
Afonso Henrique <89816770+aefonso@users.noreply.github.com>
2022-03-20 14:16:17 -0300

    modificando os campos do order

--- order.php
+++ order.php
@@ -2,23 +2,22 @@
 <?php include('partials-front/menu.php'); ?>
 
     <?php 
-        //CHeck whether food id is set or not
+       
         if(isset($_GET['food_id']))
         {
-            //Get the Food id and details of the selected food
+            
             $food_id = $_GET['food_id'];
 
-            //Get the DEtails of the SElected Food
+            
             $sql = "SELECT * FROM tbl_food WHERE id=$food_id";
-            //Execute the Query
+           
             $res = mysqli_query($conn, $sql);
-            //Count the rows
+           
             $count = mysqli_num_rows($res);
-            //CHeck whether the data is available or not
+            
             if($count==1)
             {
-                //WE Have DAta
-                //GEt the Data from Database
+             
                 $row = mysqli_fetch_assoc($res);
 
                 $title = $row['title'];
@@ -27,40 +26,39 @@
             }
             else
             {
-                //Food not Availabe
-                //REdirect to Home Page
+                
                 header('location:'.SITEURL);
             }
         }
         else
         {
-            //Redirect to homepage
+            
             header('location:'.SITEURL);
         }
     ?>
 
-    <!-- fOOD sEARCH Section Starts Here -->
+
     <section class="food-search">
         <div class="container">
             
-            <h2 class="text-center text-white">Fill this form to confirm your order.</h2>
+            <h2 class="text-center text-white">Preencha este formulário para confirmar seu pedido.</h2>
 
             <form action="" method="POST" class="order">
                 <fieldset>
-                    <legend>Selected Food</legend>
+                    <legend>Selecionar Comida</legend>
 
                     <div class="food-menu-img">
                         <?php 
                         
-                            //CHeck whether the image is available or not
+                           
                             if($image_name=="")
                             {
-                                //Image not Availabe
-                                echo "<div class='error'>Image not Available.</div>";
+                                
+                                echo "<div class='error'>Imagem indisponível.</div>";
                             }
                             else
                             {
-                                //Image is Available
+                               
                                 ?>
                                 <img src="<?php echo SITEURL; ?>images/food/<?php echo $image_name; ?>" alt="Chicke Hawain Pizza" class="img-responsive img-curve">
                                 <?php
@@ -74,10 +72,10 @@
                         <h3><?php echo $title; ?></h3>
                         <input type="hidden" name="food" value="<?php echo $title; ?>">
 
-                        <p class="food-price">$<?php echo $price; ?></p>
+                        <p class="food-price">R$<?php echo $price; ?></p>
                         <input type="hidden" name="price" value="<?php echo $price; ?>">
 
-                        <div class="order-label">Quantity</div>
+                        <div class="order-label">Quantidade</div>
                         <input type="number" name="qty" class="input-responsive" value="1" required>
                         
                     </div>
@@ -85,20 +83,20 @@
                 </fieldset>
                 
                 <fieldset>
-                    <legend>Delivery Details</legend>
-                    <div class="order-label">Full Name</div>
+                    <legend>Detalhes do Pedido</legend>
+                    <div class="order-label">Nome completo</div>
                     <input type="text" name="full-name" placeholder="E.g. Vijay Thapa" class="input-responsive" required>
 
-                    <div class="order-label">Phone Number</div>
+                    <div class="order-label">Número de telefone</div>
                     <input type="tel" name="contact" placeholder="E.g. 9843xxxxxx" class="input-responsive" required>
 
                     <div class="order-label">Email</div>
                     <input type="email" name="email" placeholder="E.g. hi@vijaythapa.com" class="input-responsive" required>
 
-                    <div class="order-label">Address</div>
+                    <div class="order-label">Endereço</div>
                     <textarea name="address" rows="10" placeholder="E.g. Street, City, Country" class="input-responsive" required></textarea>
 
-                    <input type="submit" name="submit" value="Confirm Order" class="btn btn-primary">
+                    <input type="submit" name="submit" value="Confirma Pedido" class="btn btn-primary">
                 </fieldset>
 
             </form>
@@ -107,6 +105,6 @@
 
         </div>
     </section>
-    <!-- fOOD sEARCH Section Ends Here -->
+   
 
     <?php include('partials-front/footer.php'); ?>
