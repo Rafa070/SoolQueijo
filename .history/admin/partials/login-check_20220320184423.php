45ba9177cd946c82a2b78f78ed3d5f5414ab3e08
Rafael Ribeiro <rafaelribeirorn5@gmail.com>
2022-03-21 15:14:31 -0300

    config post

--- .history/admin/partials/login-check_20220320184423.php
+++
@@ -1,9 +0,0 @@
-<?php 
-
-    if(!isset($_SESSION['username']))
-    {
-        $_SESSION['no-login-message'] = "<div class='error text-center'>Erro de Autenticação</div>";
-        header('location:'.SITEURL.'admin/login.php');
-    }
-
-?>
