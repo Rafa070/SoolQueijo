406cfaefd6151f63e136147d03a5794d7553898f
Rafael Ribeiro <rafaelribeirorn5@gmail.com>
2022-03-21 15:13:48 -0300

    config css

--- .history/partials-front/menu_20220321151046.php
+++ .history/partials-front/menu_20220321151046.php
@@ -7,6 +7,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>SoolQueijo</title>
+
+
     <link rel="stylesheet" href="css/style.css">
 </head>
 
@@ -23,7 +25,7 @@
             <div class="menu text-right">
                 <ul>
                     <li>
-                        <a href="<?php echo SITEURL; ?>">Início</a>
+                        <a href="<?php echo SITEURL; ?>">nício</a>
                     </li>
                     <li>
                         <a href="<?php echo SITEURL; ?>categories.php">Categorias</a>
