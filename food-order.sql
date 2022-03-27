ed4f74b287bf2a589aa87f89415ff89ae9d906d9
Rafael Ribeiro <rafaelribeirorn5@gmail.com>
2022-03-26 22:45:52 -0300

    ajustes no banco

--- food-order.sql
+++ food-order.sql
@@ -3,13 +3,14 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 20-Mar-2022 às 23:00
+-- Tempo de geração: 27-Mar-2022 às 01:41
 -- Versão do servidor: 10.4.22-MariaDB
 -- versão do PHP: 8.1.2
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
+
 
 --
 -- Banco de dados: `food-order`
@@ -33,9 +34,9 @@ CREATE TABLE `tbl_admin` (
 --
 
 INSERT INTO `tbl_admin` (`id`, `full_name`, `username`, `password`) VALUES
-(18, 'Rafael Ribeiro', 'rafael', '496cfe52debb9a3c40847aaf72b7e179'),
 (21, 'Afonso Santos', 'afonso123', 'cf943a0d9eda852e2b4ae2589b39fcb0'),
-(22, 'Monik Gomes', 'monik123', '21232f297a57a5a743894a0e4a801fc3');
+(24, 'Rafael Ribeiro', 'rafael123', '496cfe52debb9a3c40847aaf72b7e179'),
+(25, 'Carla Gomes', 'monik123', 'c8a263887e20d0a8a6e47e410bc14ee9');
 
 -- --------------------------------------------------------
 
@@ -56,9 +57,9 @@ CREATE TABLE `tbl_category` (
 --
 
 INSERT INTO `tbl_category` (`id`, `title`, `image_name`, `featured`, `active`) VALUES
-(4, 'Pizza', 'Food_Category_790.jpg', 'Yes', 'Yes'),
-(5, 'Burger', 'Food_Category_344.jpg', 'Yes', 'Yes'),
-(9, 'Coxinha', 'Food_Category_4.jpg', 'Yes', 'Yes');
+(16, 'Pizza', 'Food_Category_499.jpg', 'Sim', 'Sim'),
+(17, 'Burger', 'Food_Category_349.jpg', 'Sim', 'Sim'),
+(18, 'Coxinha', 'Food_Category_56.jpg', 'Sim', 'Sim');
 
 -- --------------------------------------------------------
 
@@ -82,12 +83,16 @@ CREATE TABLE `tbl_food` (
 --
 
 INSERT INTO `tbl_food` (`id`, `title`, `description`, `price`, `image_name`, `category_id`, `featured`, `active`) VALUES
-(9, 'Coxinha', 'Coxinha de catupiry', '9.00', 'Food-Name-659.jpg', 9, 'Yes', 'Yes'),
-(10, 'Coxinha', 'Coxinha de Frango', '8.00', 'Food-Name-5034.jpg', 9, 'Yes', 'Yes'),
-(11, 'Pizza', 'Pizza queijo', '48.00', 'Food-Name-3815.jpg', 4, 'Yes', 'Yes'),
-(12, 'Pizza ', 'Pizza de Calabresa', '49.00', 'Food-Name-1242.jpg', 4, 'Yes', 'Yes'),
-(13, 'Burger', 'Podrão', '29.00', 'Food-Name-2814.jpg', 5, 'Yes', 'Yes'),
-(14, 'Burger', 'Hambúrguer text', '19.00', 'Food-Name-576.jpg', 5, 'Yes', 'Yes');
+(19, 'Coxinha', 'Coxinha de Frango', '6.00', 'Food-Name-33.jpg', 18, 'Sim', 'Sim'),
+(20, 'Pizza', 'Pizza de Queijo', '41.00', 'Food-Name-7808.jpg', 0, 'Sim', 'Sim'),
+(23, 'Hambúrguer', 'X-tudo caseiro com carne de bovina, alface, tomate, queijo, bacon e molho da casa.', '14.00', 'Food-Name-1795.jpg', 16, 'Sim', 'Sim'),
+(24, 'Pizza', 'Pizza de azeitona', '39.00', 'Food-Name-5347.jpg', 16, 'Sim', 'Sim'),
+(25, 'Hambúrguer', 'Simples da casa', '19.00', 'Food-Name-641.jpg', 0, 'Sim', 'Sim'),
+(26, 'Coxinha', 'Coxinha de camarão', '25.00', 'Food-Name-6233.jpg', 16, 'Sim', 'Sim');
+
+--
+-- Índices para tabelas despejadas
+--
 
 --
 -- Índices para tabela `tbl_admin`
@@ -108,20 +113,24 @@ ALTER TABLE `tbl_food`
   ADD PRIMARY KEY (`id`);
 
 --
+-- AUTO_INCREMENT de tabelas despejadas
+--
+
+--
 -- AUTO_INCREMENT de tabela `tbl_admin`
 --
 ALTER TABLE `tbl_admin`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
 
 --
 -- AUTO_INCREMENT de tabela `tbl_category`
 --
 ALTER TABLE `tbl_category`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+
 --
 -- AUTO_INCREMENT de tabela `tbl_food`
 --
 ALTER TABLE `tbl_food`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
 COMMIT;
-
